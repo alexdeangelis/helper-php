a579488b4d9f558ebf74729fa1a396d5c8120dc1
Alex <alex.deangelis@goodmanfox.com>
2018-10-12 09:15:54 +0100

    loop update

--- basic_loop.php
+++ basic_loop.php
@@ -1,6 +1,23 @@
+<?php //THE SHORTENED LOOP ?>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 
 <?php endwhile; else : ?>
 	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
+
+
+
+<?php //THE LONGFORM LOOP ?>
+
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+		//
+		// Post Content here
+		//
+	} // end while
+} // end if
+?>
