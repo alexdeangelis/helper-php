23a4748239bbbb6b4bdb960f61bbb04b2cdbcfba
Alex <alex.deangelis@goodmanfox.com>
2018-11-02 17:12:58 +0000

    get page id by slug

--- get_page_id_by_slug.php
+++ get_page_id_by_slug.php
@@ -1,5 +1,9 @@
 <?php
 
+//This is for a page
 $page_id = get_page_by_path( 'about' );
+
+//This is for a custom post type
+$single_post = get_page_by_path( 'single-post', OBJECT, 'layouts' );
 
 ?>
