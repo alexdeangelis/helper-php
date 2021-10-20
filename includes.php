d62591b54c2f9a23faa5ed765e8eb69c870d39e8
Alex De Angelis <alexdeangelis86@googlemail.com>
2021-10-20 10:30:16 +0100

    extra include style

--- includes.php
+++ includes.php
@@ -1,1 +1,7 @@
-<?php include __DIR__ . '/_templates/modal_help.php'; ?>
+<?php
+
+// Theme Include
+include __DIR__ . '/_templates/modal_help.php';
+
+// Plugin Include
+include( plugin_dir_path( __FILE__ ) . '/shortcodes/pubs/address.php');
